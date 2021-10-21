9d170e6ea8925dfb785df1bcee3a6f56dd636c1a
veerlorden <7z95A0dJ73h213>
2021-10-22 00:12:48 +0300

    Initial Commit

---
+++ CurrentsNews/CurrentsNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentsNewsApp.swift
+//  CurrentsNews
+//
+//  Created by Вадим Буркин on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CurrentsNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
